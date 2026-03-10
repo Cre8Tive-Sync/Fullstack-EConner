daa2103a0c0c7557af379e133de65d30d58c5a0a
HansongProgramming <floreshansharoldlee@gmail.com>
2026-03-10 18:38:43 +0800

    camera and scene fully set to run

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   plugins: [
+    basicSsl(),
     react(),
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
