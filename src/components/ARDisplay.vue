6d32fbc74759ca50fc8122fd3d2c313afe1624dc
HansongProgramming <floreshansharoldlee@gmail.com>
2026-03-10 17:34:28 +0800

    added simple mount

---
+++ src/components/ARDisplay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div ref="arMount" style="width: 100%; height: 100vh;" />
+</template>
+
+<script>
+import { createRoot } from 'react-dom/client'
+import { createElement } from 'react'
+import ARScene from './ar/ARScene'
+
+export default {
+  mounted() {
+    this._arRoot = createRoot(this.$refs.arMount)
+    this._arRoot.render(createElement(ARScene))
+  },
+  beforeUnmount() {
+    this._arRoot.unmount()
+  }
+}
+</script>
