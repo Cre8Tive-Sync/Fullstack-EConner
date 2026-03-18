719fa125b53eb99b9a8717b577143cb057073d37
nervachan <nervagrf@gmail.com>
2026-03-18 10:17:10 +0800

    test fixes

--- src/components/ARDisplay.vue
+++ src/components/ARDisplay.vue
@@ -2,19 +2,26 @@
   <div ref="arMount" style="width: 100%; height: 100vh;" />
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from 'vue'
 import { createRoot } from 'react-dom/client'
+import type { Root } from 'react-dom/client'
 import { createElement } from 'react'
 
-export default {
+export default defineComponent({
+  data() {
+    return {
+      _arRoot: null as Root | null
+    }
+  },
   mounted() {
     import('./ar/ARScene.jsx').then(({ default: ARScene }) => {
-      this._arRoot = createRoot(this.$refs.arMount)
+      this._arRoot = createRoot(this.$refs.arMount as Element)
       this._arRoot.render(createElement(ARScene))
     })
   },
   beforeUnmount() {
     this._arRoot?.unmount()
   }
-}
+})
 </script>
